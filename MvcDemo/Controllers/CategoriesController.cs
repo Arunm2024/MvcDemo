876ae074ab4833319695190fc40cba877fd391d9
Arunm2024 <arunmehta1785@gmail.com>
2024-03-14 18:25:36 +0530

    change in demo file

--- MvcDemo/Controllers/CategoriesController.cs
+++ MvcDemo/Controllers/CategoriesController.cs
@@ -7,11 +7,22 @@ namespace MvcDemo.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var categories = CategoriesRepository.GetCategory();
+            return View(categories);
         }
         public IActionResult Edit(int? id)
         {
-            var category= new Category() { CategoryId=id.HasValue ? id.Value : 0 };
+            var category= CategoriesRepository.GetCategoryById(id.HasValue ? id.Value : 0);
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category) 
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.UpdateCategory(category.CategoryId, category);
+                return RedirectToAction(nameof(Index));
+            }
             return View(category);
         }
     }
--- MvcDemo/Models/Category.cs
+++ MvcDemo/Models/Category.cs
@@ -1,8 +1,11 @@
-﻿namespace MvcDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcDemo.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required]
         public string Name { get; set; }= string.Empty;
         public string Description { get; set; } = string.Empty;
 
